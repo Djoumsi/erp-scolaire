3c83d6e5f457231ea1006fc67bc47ee67cc02477
Haizen <vinilbrayan09@gmail.com>
2026-05-22 13:46:56 +0200

    Configure database for PostgreSQL

--- config/database.php
+++ config/database.php
@@ -1,9 +1,10 @@
 <?php
 return [
     'host'     => $_ENV['DB_HOST']     ?? 'localhost',
-    'port'     => $_ENV['DB_PORT']     ?? '3306',
+    'port'     => $_ENV['DB_PORT']     ?? '5432',
     'database' => $_ENV['DB_DATABASE'] ?? 'erp_scolaire',
-    'username' => $_ENV['DB_USERNAME'] ?? 'root',
-    'password' => $_ENV['DB_PASSWORD'] ?? '',
-    'charset'  => 'utf8mb4',
+    'username' => $_ENV['DB_USERNAME'] ?? 'postgres',
+    'password' => $_ENV['DB_PASSWORD'] ?? 'PbYmK7IKPQjoxAQZEQJeeJe0CNBbGZVQ',
+    'charset'  => 'utf8',
+    'driver'   => 'pgsql'
 ];
